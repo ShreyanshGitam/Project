91d5c3ab83ec0de81e655f49d40c4ceda02180a5
ShreyanshGitam <112847701+ShreyanshGitam@users.noreply.github.com>
2024-07-15 22:14:25 +0530

    Update file_sharer.py

--- file_sharer.py
+++ file_sharer.py
@@ -22,8 +22,7 @@ STORAGE_DIR = "storage"
 codes = {}
 current_user = None
 current_path = None
-selected_file_path = None
-selected_folder_path = None
+selected_files = set()
 move_mode = False
 
 # Sqlite3 Database setup
@@ -146,7 +145,7 @@ def login_window():
         if authenticate(username, password):
             current_user = username
             root.destroy()
-            file_management_window()  # Directly open file management window after login
+            file_management_window()
         else:
             messagebox.showerror("Login Failed", "Invalid username or password")
 
@@ -186,7 +185,6 @@ def register_window():
             messagebox.showerror("Error", "Username already exists")
 
     Button(window, text="Register", width=10, height=1, bg="green", fg="white", command=register_user).place(x=130, y=150)
-
     window.mainloop()
 
 
@@ -198,7 +196,7 @@ def generate_aes_key():
     return os.urandom(16)
 
 
-def send_file(new_key, key_label):
+def send_files(new_key, key_label, selected_files):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -207,79 +205,94 @@ def send_file(new_key, key_label):
         s.bind((host, port))
         s.listen(1)
         conn, addr = s.accept()
-        with open(selected_file_path, 'rb') as file:
-            file_data = file.read()
-        decrypted_data = decrypt(file_data, KEY)
-        re_encrypted_data = encrypt(decrypted_data, new_key)
 
-        # Send the file name and size first
-        file_name = os.path.basename(selected_file_path)
-        file_size = len(re_encrypted_data)
-        conn.sendall(f"{file_name}\n{file_size}\n".encode())
-        # Then send the file data
-        conn.sendall(re_encrypted_data)
+        for selected_file_path in selected_files:
+            with open(selected_file_path, 'rb') as file:
+                file_data = file.read()
+            decrypted_data = decrypt(file_data, KEY)
+            re_encrypted_data = encrypt(decrypted_data, new_key)
+
+            # Send the file name and size first
+            file_name = os.path.basename(selected_file_path)
+            file_size = len(re_encrypted_data)
+            conn.sendall(f"{file_name}\n{file_size}\n".encode())
+            # Then send the file data
+            conn.sendall(re_encrypted_data)
+
         conn.close()
         s.close()
         key_label.config(text=f"New AES Key: {new_key.hex()}")
-        messagebox.showinfo("Success", "File has been transmitted successfully!")
+        messagebox.showinfo("Success", "Files have been transmitted successfully!")
     except Exception as e:
         messagebox.showerror("Error", f"An error occurred: {e}")
 
 def send_window():
-    window = Toplevel(root)
-    window.title("Send")
-    window.geometry('400x300')
-    center_window(window, 400, 300)
-    window.configure(bg="#1e1e1e")
-    window.resizable(False, False)
+    if selected_files:
+        window = Toplevel(root)
+        window.title("Send")
+        window.geometry('400x300')
+        center_window(window, 400, 300)
+        window.configure(bg="#1e1e1e")
+        window.resizable(False, False)
 
-    new_key = generate_aes_key()
-    key_label_text = f"New AES Key: {new_key.hex()}"
+        new_key = generate_aes_key()
+        key_label_text = f"New AES Key: {new_key.hex()}"
 
-    def start_sending():
-        threading.Thread(target=send_file, args=(new_key, key_label)).start()
+        def start_sending():
+            threading.Thread(target=send_files, args=(new_key, key_label, list(selected_files))).start()
 
-    def copy_to_clipboard():
-        root.clipboard_clear()
-        root.clipboard_append(new_key.hex())
-        root.update()  # Keeps the clipboard contents after the window is closed
-        messagebox.showinfo("Copied", "AES Key copied to clipboard")
+        def copy_to_clipboard():
+            root.clipboard_clear()
+            root.clipboard_append(new_key.hex())
+            root.update()  # Keeps the clipboard contents after the window is closed
+            messagebox.showinfo("Copied", "AES Key copied to clipboard")
 
-    host = socket.gethostname()
-    Label(window, text=f'Your Device ID: {host}', font='Verdana 12 bold', bg='#1e1e1e', fg='white').place(x=10, y=10)
-    file_label = Label(window, text="Selected file: " + (selected_file_path if selected_file_path else "No file selected"), bg='#1e1e1e', fg='white')
-    file_label.place(x=10, y=50)
-    Button(window, text="SEND", width=12, height=1, font='arial 12 bold', bg='#000', fg="#fff", command=start_sending).place(x=10, y=80)
-    key_label = Label(window, text=key_label_text, bg='#1e1e1e', fg='white')
-    key_label.place(x=10, y=120)
-    Button(window, text="Copy Key", width=12, height=1, font='arial 12 bold', bg='#007ACC', fg='#fff', command=copy_to_clipboard).place(x=10, y=160)
-    window.mainloop()
+        host = socket.gethostname()
+        Label(window, text=f'Your Device ID: {host}', font='Verdana 12 bold', bg='#1e1e1e', fg='white').place(x=10, y=10)
+        file_label = Label(window, text="Selected files: " + (", ".join([os.path.basename(f) for f in selected_files]) if selected_files else "No files selected"), bg='#1e1e1e', fg='white')
+        file_label.place(x=10, y=50)
+        Button(window, text="SEND", width=12, height=1, font='arial 12 bold', bg='#000', fg="#fff", command=start_sending).place(x=10, y=80)
+        key_label = Label(window, text=key_label_text, bg='#1e1e1e', fg='white')
+        key_label.place(x=10, y=120)
+        Button(window, text="Copy Key", width=12, height=1, font='arial 12 bold', bg='#007ACC', fg='#fff', command=copy_to_clipboard).place(x=10, y=160)
+        window.mainloop()
 
 
-def receive_file(sender_id):
+
+def receive_files(sender_id):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         port = 8080
         s.connect((sender_id, port))
-        # Receive the file name and size first
-        header = s.recv(1024).split(b'\n')
-        file_name = header[0].decode()
-        file_size = int(header[1].decode())
-        all_data = b""
-        while len(all_data) < file_size:
-            data = s.recv(1024)
-            if not data:
-                break
-            all_data += data
-        s.close()
+
         user_dir = os.path.join(STORAGE_DIR, f"{current_user}_dir", "received_files")
         if not os.path.exists(user_dir):
             os.makedirs(user_dir)
-        filepath = os.path.join(user_dir, file_name)
-        with open(filepath, 'wb') as file:
-            file.write(all_data)
-        messagebox.showinfo("Success", f"File has been received and saved as {file_name}")
-        ask_for_key(filepath)
+
+        while True:
+            # Receive the file name and size first
+            header = s.recv(1024).split(b'\n')
+            if len(header) < 2:
+                break
+            file_name = header[0].decode()
+            file_size = int(header[1].decode())
+            all_data = b""
+            while len(all_data) < file_size:
+                data = s.recv(1024)
+                if not data:
+                    break
+                all_data += data
+
+            if not all_data:
+                break
+
+            filepath = os.path.join(user_dir, file_name)
+            with open(filepath, 'wb') as file:
+                file.write(all_data)
+            messagebox.showinfo("Success", f"File has been received and saved as {file_name}")
+            ask_for_key(filepath)
+
+        s.close()
     except socket.gaierror:
         messagebox.showerror("Error", "Hostname could not be resolved. Please check the sender's Device ID.")
     except Exception as e:
@@ -295,7 +308,7 @@ def receive_window():
 
     def start_receiving():
         sender_id = senderID.get()
-        threading.Thread(target=receive_file, args=(sender_id,)).start()
+        threading.Thread(target=receive_files, args=(sender_id,)).start()
 
     Label(window, text="Receive Window", font=('Arial', 20), bg="#1e1e1e", fg="#ffffff").place(x=110, y=0)
     Label(window, text="Input sender ID", font=('arial', 10, 'bold'), bg="#1e1e1e", fg="#ffffff").place(x=20, y=50)
@@ -341,8 +354,9 @@ def ask_for_key(filepath):
     key_window.mainloop()
 
 
+
 def file_management_window():
-    global root, current_path, selected_file_path, selected_folder_path, move_mode
+    global root, current_path, selected_files, move_mode
     root = Tk()
     root.title("File Management")
     root.geometry('1100x600')
@@ -353,11 +367,48 @@ def file_management_window():
     Label(root, text="File Management", font=('Arial', 20), bg="#1e1e1e", fg="#ffffff").pack(pady=10)
 
     current_path = os.path.join(STORAGE_DIR, f"{current_user}_dir")
-    selected_file_path = None
-    selected_folder_path = None
+    selected_files = set()
     move_mode = False
 
     load_images()
+
+    def custom_rename_dialog(initial_value):
+        dialog = Toplevel(root)
+        dialog.title("Rename File")
+        dialog.geometry("400x200")  # Set the desired size
+        center_window(dialog, 350, 135)
+        dialog.configure(bg="#1e1e1e")
+        dialog.resizable(False, False)
+
+        Label(dialog, text="Enter the New Name:", bg="#1e1e1e", fg="#ffffff").pack(pady=10)
+        new_name_var = StringVar(dialog, value=initial_value)
+        new_name_entry = Entry(dialog, textvariable=new_name_var, width=40)
+        new_name_entry.pack(pady=10)
+        new_name_entry.focus()
+
+        def on_submit():
+            dialog.new_name = new_name_var.get()
+            dialog.destroy()
+
+        Button(dialog, text="Rename", command=on_submit, bg="green", fg="white").pack(pady=10)
+
+        dialog.transient(root)  # Ensure the dialog stays on top of the root window
+        dialog.grab_set()  # Ensure all input is directed to this dialog
+        root.wait_window(dialog)  # Wait until the dialog is closed
+
+        return getattr(dialog, 'new_name', None)
+
+    def rename_file():
+        if selected_files and len(selected_files) == 1:
+            selected_file_path = list(selected_files)[0]
+            new_name = custom_rename_dialog(initial_value=os.path.basename(selected_file_path))
+            if new_name:
+                new_path = os.path.join(current_path, new_name)
+                os.rename(selected_file_path, new_path)
+                selected_files.clear()
+                selected_files.add(new_path)
+                load_files(current_path)
+                messagebox.showinfo("Success", f"File has been renamed to {new_name}")
 
     def load_files(path):
         for widget in file_display_frame.winfo_children():
@@ -407,27 +458,23 @@ def file_management_window():
             col += 1
 
     def on_select_file(event, filepath):
-        global selected_file_path, selected_folder_path, move_mode
+        global selected_files, move_mode
         if not move_mode:
-            selected_file_path = filepath
-            selected_folder_path = None
-            for widget in file_display_frame.winfo_children():
-                widget.config(bg="#1e1e1e", fg="white")
-            event.widget.config(bg="blue", fg="yellow")
-            move_button.config(state=NORMAL)
-
-    def on_select_folder(event, filepath):
-        global selected_folder_path, selected_file_path, move_mode
-        if move_mode:
-            selected_folder_path = filepath
-            for widget in file_display_frame.winfo_children():
-                widget.config(bg="#1e1e1e", fg="white")
-            event.widget.config(bg="green", fg="yellow")
-        else:
-            selected_folder_path = filepath
-            for widget in file_display_frame.winfo_children():
-                widget.config(bg="#1e1e1e", fg="white")
-            event.widget.config(bg="green", fg="yellow")
+            if event.state & 0x0001:  # Shift key is held down
+                if filepath in selected_files:
+                    selected_files.remove(filepath)
+                    event.widget.config(bg="#1e1e1e", fg="white")
+                else:
+                    selected_files.add(filepath)
+                    event.widget.config(bg="blue", fg="yellow")
+            else:
+                if filepath in selected_files:
+                    selected_files.remove(filepath)
+                    event.widget.config(bg="#1e1e1e", fg="white")
+                else:
+                    selected_files.add(filepath)
+                    event.widget.config(bg="blue", fg="yellow")
+            move_button.config(state=NORMAL if selected_files else DISABLED)
 
     def view_folder(path):
         global current_path, move_mode
@@ -452,18 +499,12 @@ def file_management_window():
             move_button.config(state=NORMAL)
 
     def delete_file():
-        if selected_file_path:
-            os.remove(selected_file_path)
+        if selected_files:
+            for file in selected_files:
+                os.remove(file)
             load_files(current_path)
-            messagebox.showinfo("Success", f"File {os.path.basename(selected_file_path)} has been deleted")
-
-    def rename_file():
-        if selected_file_path:
-            new_name = simpledialog.askstring("Rename File", "Enter new name for the file:", initialvalue=os.path.basename(selected_file_path))
-            if new_name:
-                os.rename(selected_file_path, os.path.join(current_path, new_name))
-                load_files(current_path)
-                messagebox.showinfo("Success", f"File has been renamed to {new_name}")
+            messagebox.showinfo("Success", "Selected files have been deleted")
+            selected_files.clear()
 
     def create_folder():
         new_folder_name = simpledialog.askstring("Create Folder", "Enter name for the new folder:")
@@ -488,9 +529,10 @@ def file_management_window():
                 messagebox.showerror("Error", f"An error occurred: {e}")
 
     def download_file():
-        if selected_file_path:
-            save_path = filedialog.asksaveasfilename(initialdir=os.getcwd(), title='Save File As', initialfile=os.path.basename(selected_file_path))
-            if save_path:
+        if selected_files:
+            downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads")
+            for selected_file_path in selected_files:
+                save_path = os.path.join(downloads_folder, os.path.basename(selected_file_path))
                 try:
                     with open(selected_file_path, 'rb') as fsrc:
                         encrypted_data = fsrc.read()
@@ -498,23 +540,49 @@ def file_management_window():
                         decrypted_data = decrypt(encrypted_data, KEY)
                         with open(save_path, 'wb') as fdst:
                             fdst.write(decrypted_data)
-                        messagebox.showinfo("Success", "File has been decrypted and downloaded successfully")
+                        messagebox.showinfo("Success",
+                                            f"File has been decrypted and downloaded to {save_path} successfully")
                     except ValueError as e:
                         messagebox.showerror("Error", "Decryption failed. Invalid padding bytes.")
                 except Exception as e:
                     messagebox.showerror("Error", f"An error occurred: {e}")
 
     def move_file():
-        global selected_file_path, selected_folder_path, move_mode, current_path
-        if not move_mode and selected_file_path:
+        global selected_files, move_mode, current_path
+
+        def disable_buttons():
+            refresh_button.config(state=DISABLED)
+            delete_button.config(state=DISABLED)
+            rename_button.config(state=DISABLED)
+            create_folder_button.config(state=DISABLED)
+            upload_button.config(state=DISABLED)
+            download_button.config(state=DISABLED)
+            send_button.config(state=DISABLED)
+            receive_button.config(state=DISABLED)
+
+        def enable_buttons():
+            refresh_button.config(state=NORMAL)
+            delete_button.config(state=NORMAL)
+            rename_button.config(state=NORMAL)
+            create_folder_button.config(state=NORMAL)
+            upload_button.config(state=NORMAL)
+            download_button.config(state=NORMAL)
+            send_button.config(state=NORMAL)
+            receive_button.config(state=NORMAL)
+            if current_path == os.path.join(STORAGE_DIR, f"{current_user}_dir"):
+                back_button.config(state=DISABLED, bg="gray")
+            else:
+                back_button.config(state=NORMAL, bg="red")
+
+        if not move_mode and selected_files:
             move_mode = True
             move_button.config(text="Move Here")
             disable_buttons()
-        elif move_mode and selected_file_path:
+        elif move_mode and selected_files:
             try:
-                shutil.move(selected_file_path, os.path.join(current_path, os.path.basename(selected_file_path)))
-                selected_file_path = None
-                selected_folder_path = None
+                for file in selected_files:
+                    shutil.move(file, os.path.join(current_path, os.path.basename(file)))
+                selected_files.clear()
                 move_mode = False
                 move_button.config(text="Move")
                 load_files(current_path)
@@ -522,29 +590,6 @@ def file_management_window():
             except Exception as e:
                 messagebox.showerror("Error", f"An error occurred: {e}")
 
-    def disable_buttons():
-        refresh_button.config(state=DISABLED)
-        delete_button.config(state=DISABLED)
-        rename_button.config(state=DISABLED)
-        create_folder_button.config(state=DISABLED)
-        upload_button.config(state=DISABLED)
-        download_button.config(state=DISABLED)
-        send_button.config(state=DISABLED)
-        receive_button.config(state=DISABLED)
-
-    def enable_buttons():
-        refresh_button.config(state=NORMAL)
-        delete_button.config(state=NORMAL)
-        rename_button.config(state=NORMAL)
-        create_folder_button.config(state=NORMAL)
-        upload_button.config(state=NORMAL)
-        download_button.config(state=NORMAL)
-        send_button.config(state=NORMAL)
-        receive_button.config(state=NORMAL)
-        if current_path == os.path.join(STORAGE_DIR, f"{current_user}_dir"):
-            back_button.config(state=DISABLED, bg="gray")
-        else:
-            back_button.config(state=NORMAL, bg="red")
 
     button_frame = Frame(root, bg="#1e1e1e")
     button_frame.pack(pady=10)
